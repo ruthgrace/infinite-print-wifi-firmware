bdaa998c161356af5289d136207239edbc2d4c7a
ruthgrace <ruthgracewong@gmail.com>
2020-07-07 15:54:00 -0700

    add platformio project with wifi connection tutorial code

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <ESP8266WiFi.h>
+
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+void setup()
+{
+  Serial.begin(9600);
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("NodeMCU-Arduino-PlatformIO");
+  Serial.println("Connected!");
+}
+
+void loop()
+{
+  Serial.println("Idle...");
+  delay(1000);
+}
